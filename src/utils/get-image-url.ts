28c5ee6d314d9f595f0af673131da6e5f1a0e6eb
devferx <ferkki@hotmail.com>
2024-11-23 22:09:38 -0400

    Fix getImageUrl function to correctly format image URL path

--- src/utils/get-image-url.ts
+++ src/utils/get-image-url.ts
@@ -1,4 +1,4 @@
 export const getImageUrl = (
   path: string,
   size: 'original' | 'w780' | 'w500' | 'w300' = 'w500',
-) => `https://image.tmdb.org/t/p/${size}/${path}`
+) => `https://image.tmdb.org/t/p/${size}${path}`
